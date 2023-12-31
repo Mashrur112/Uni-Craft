44ac7a96d9ab32959532634488065d199a47cab1
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2024-01-01 03:47:42 +0600

    Time Planner in Calender

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:timeplanner/timeplanner.dart';
+import 'package:uni_craft/timeplanner.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
