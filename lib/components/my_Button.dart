635e0ca9be3bf3c245dc222b1808c9e70fb5266a
ahnuf <ahnufkarimchowdhury@gmail.com>
2024-01-14 05:27:42 +0600

    component 2

---
+++ lib/components/my_Button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final void Function()? onTap;
+  final String text;
+
+  const MyButton({
+    Key? key,
+    required this.onTap,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(9),
+        decoration: BoxDecoration(
+          color: Colors.black,borderRadius: BorderRadius.circular(25),
+        ),
+        child: Center(
+          child: Text(text,
+          style: const TextStyle(color:Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+}
