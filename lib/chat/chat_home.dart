987f27b357d260daeaf44751a0864d03c181e768
ahnuf <ahnufkarimchowdhury@gmail.com>
2024-01-14 06:00:44 +0600

    update chat home 2

--- lib/chat/chat_home.dart
+++ lib/chat/chat_home.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'chat_page.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class ChatHome extends StatefulWidget {
+  const ChatHome({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<ChatHome> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<ChatHome> {
   //instance of auth
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
